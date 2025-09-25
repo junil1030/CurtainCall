df478255a20bf9659bea356aa4880c2f22201eb7
junil1030 <dccrdseo@naver.com>
2025-09-25 14:45:17 +0900

    feat: Init Project
    - Codebase 전환
    - 폴더 정리

---
+++ CurtainCall/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurtainCall
+//
+//  Created by 서준일 on 9/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+
+}
+
