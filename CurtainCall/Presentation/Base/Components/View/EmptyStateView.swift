bdcfdc79873d7051e38185bf9907b4f99fde06d9
junil1030 <dccrdseo@naver.com>
2025-10-04 11:17:56 +0900

    feat: 재사용 가능한 EmptyStateView 컴포넌트 추가
    - 아이콘과 메시지를 표시하는 빈 상태 뷰
    - configure 메서드로 외부에서 커스터마이징 가능
    - 찜한 공연, 검색 결과 등 다양한 화면에서 재사용 가능

--- CurtainCall/Presentation/Base/Components/View/EmptyStateView.swift
+++ CurtainCall/Presentation/Base/Components/View/EmptyStateView.swift
@@ -5,4 +5,62 @@
 //  Created by 서준일 on 10/4/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class EmptyStateView: BaseView {
+    
+    // MARK: - UI Components
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .ccSecondaryText
+        return imageView
+    }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.font = .ccBody
+        label.textColor = .ccSecondaryText
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 16
+        stack.alignment = .center
+        return stack
+    }()
+    
+    // MARK: - BaseView Override Methods
+    override func setupHierarchy() {
+        addSubview(stackView)
+        stackView.addArrangedSubview(iconImageView)
+        stackView.addArrangedSubview(messageLabel)
+    }
+    
+    override func setupLayout() {
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(32)
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(80)
+        }
+    }
+    
+    override func setupStyle() {
+        super.setupStyle()
+        backgroundColor = .clear
+    }
+    
+    // MARK: - Configure
+    func configure(icon: UIImage?, message: String) {
+        iconImageView.image = icon
+        messageLabel.text = message
+    }
+}
