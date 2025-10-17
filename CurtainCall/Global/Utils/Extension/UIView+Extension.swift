22a8c66cee2c6ae6d19420a4205abca4244913f9
junil1030 <dccrdseo@naver.com>
2025-10-18 00:22:38 +0900

    feat: 기존에 UIImageView에서 다루었던 Gradient를 UIView에서 삭제 후 추가하도록 로직 추가

--- CurtainCall/Global/Utils/Extension/UIView+Extension.swift
+++ CurtainCall/Global/Utils/Extension/UIView+Extension.swift
@@ -5,4 +5,23 @@
 //  Created by 서준일 on 10/17/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func addBottomGradient() {
+        layer.sublayers?.forEach { sublayer in
+            if sublayer is CAGradientLayer {
+                sublayer.removeFromSuperlayer()
+            }
+        }
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.7).cgColor
+        ]
+        gradient.locations = [0.5, 1.0]
+        layer.addSublayer(gradient)
+    }
+}
