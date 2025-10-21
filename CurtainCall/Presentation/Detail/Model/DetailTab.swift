0367a798bdc045d29d8d8006066411bdb5b770a3
junil1030 <dccrdseo@naver.com>
2025-10-21 09:24:47 +0900

    feat: DetailTab enum 추가
    - 4개 탭 타입 정의 (공연정보/예매정보/출연진/제작정보)
    - title 프로퍼티로 UI 텍스트 제공

--- CurtainCall/Presentation/Detail/Model/DetailTab.swift
+++ CurtainCall/Presentation/Detail/Model/DetailTab.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum DetailTab: Int, CaseIterable {
+    case info
+    case booking
+    case cast
+    case production
+    
+    var title: String {
+        switch self {
+        case .info:
+            return "공연정보"
+        case .booking:
+            return "예매정보"
+        case .cast:
+            return "출연진"
+        case .production:
+            return "제작정보"
+        }
+    }
+}
