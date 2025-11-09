90c3679bf71464bcd640c9d752fb9b6b15e23386
junil1030 <dccrdseo@naver.com>
2025-11-09 18:32:37 +0900

    feat: Toast 메세지 처리를 위한 타입 enum 추가 및 BaseViewController에 생성 함수 추가

--- CurtainCall/Global/enum/ToastType.swift
+++ CurtainCall/Global/enum/ToastType.swift
@@ -5,4 +5,41 @@
 //  Created by 서준일 on 11/9/25.
 //
 
-import Foundation
+import UIKit
+
+enum ToastType {
+    case success
+    case error
+    case info
+    case warning
+    
+    var backgroundColor: UIColor {
+        switch self {
+        case .success:
+            return UIColor.systemGreen.withAlphaComponent(0.9)
+        case .error:
+            return UIColor.systemRed.withAlphaComponent(0.9)
+        case.info:
+            return UIColor.systemBlue.withAlphaComponent(0.9)
+        case .warning:
+            return UIColor.systemOrange.withAlphaComponent(0.9)
+        }
+    }
+    
+    var textColor: UIColor {
+        return .white
+    }
+    
+    var icon: String? {
+        switch self {
+        case .success:
+            return "checkmark.circle.fill"
+        case .error:
+            return "xmark.circle.fill"
+        case .info:
+            return "info.circle.fill"
+        case .warning:
+            return "exclamationmark.triangle.fill"
+        }
+    }
+}
--- CurtainCall/Presentation/Base/View/BaseViewController.swift
+++ CurtainCall/Presentation/Base/View/BaseViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class BaseViewController: UIViewController {
 
@@ -47,5 +48,67 @@ class BaseViewController: UIViewController {
     
     func setNavigationbarHidden(_ hidden: Bool) {
         navigationController?.navigationBar.isHidden = hidden
+    }
+    
+    // MARK: - Toast
+    func showToast(message: String, type: ToastType, duration: TimeInterval = 2.5) {
+        // 토스트 컨테이너 뷰
+        let toastContainer = UIView()
+        toastContainer.backgroundColor = type.backgroundColor
+        toastContainer.layer.cornerRadius = 12
+        toastContainer.clipsToBounds = true
+        toastContainer.alpha = 0
+        
+        // 아이콘 이미지뷰
+        let iconImageView = UIImageView()
+        if let iconName = type.icon {
+            iconImageView.image = UIImage(systemName: iconName)
+            iconImageView.tintColor = type.textColor
+            iconImageView.contentMode = .scaleAspectFit
+        }
+        
+        // 메시지 레이블
+        let messageLabel = UILabel()
+        messageLabel.text = message
+        messageLabel.textColor = type.textColor
+        messageLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        messageLabel.textAlignment = .left
+        messageLabel.numberOfLines = 0
+        
+        // 스택뷰 구성
+        let stackView = UIStackView(arrangedSubviews: [iconImageView, messageLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .center
+        
+        toastContainer.addSubview(stackView)
+        view.addSubview(toastContainer)
+
+        // 레이아웃 설정
+        iconImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(16)
+        }
+        
+        toastContainer.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        }
+        
+        // 애니메이션
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
+            toastContainer.alpha = 1.0
+        } completion: { _ in
+            // 자동 dismiss
+            UIView.animate(withDuration: 0.3, delay: duration, options: .curveEaseIn) {
+                toastContainer.alpha = 0
+            } completion: { _ in
+                toastContainer.removeFromSuperview()
+            }
+        }
+
     }
 }
