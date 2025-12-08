6f379e7bdd5f973f143f24489374361fff968d1e
junil1030 <dccrdseo@naver.com>
2025-12-08 15:24:55 +0900

    feat: Wdiget Extension 추가

---
+++ CurtainCallWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  CurtainCallWidget
+//
+//  Created by 서준일 on 12/8/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ CurtainCallWidget/CurtainCallWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CurtainCallWidgetBundle.swift
+//  CurtainCallWidget
+//
+//  Created by 서준일 on 12/8/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CurtainCallWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CurtainCallWidget()
+    }
+}
