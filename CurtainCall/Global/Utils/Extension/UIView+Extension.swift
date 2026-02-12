5e84a9ea7ad9777d9f89f754c5a1eb6fee901da7
junil1030 <dccrdseo@naver.com>
2026-02-12 23:57:58 +0900

    feat: UIView 스켈레톤 shimmer 애니메이션 Extension 추가   - showSkeletonShimmer/removeSkeletonShimmer/updateSkeletonFrame 메서드 구현   - CAGradientLayer 기반 좌→우 shimmer 효과   - addBottomGradient에서 스켈레톤 레이어를 제거하지 않도록 보호 처리

--- CurtainCall/Global/Utils/Extension/UIView+Extension.swift
+++ CurtainCall/Global/Utils/Extension/UIView+Extension.swift
@@ -7,10 +7,56 @@
 
 import UIKit
 
+// MARK: - Skeleton Animation
+extension UIView {
+    fileprivate static let skeletonLayerName = "skeletonShimmerLayer"
+
+    func showSkeletonShimmer() {
+        // 이미 스켈레톤이 있으면 제거 후 다시 추가
+        removeSkeletonShimmer()
+
+        let shimmerLayer = CAGradientLayer()
+        shimmerLayer.name = UIView.skeletonLayerName
+        shimmerLayer.frame = bounds
+        shimmerLayer.cornerRadius = layer.cornerRadius
+        shimmerLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        shimmerLayer.endPoint = CGPoint(x: 1, y: 0.5)
+
+        let baseColor = UIColor.systemGray5.cgColor
+        let highlightColor = UIColor.systemGray3.cgColor
+
+        shimmerLayer.colors = [baseColor, highlightColor, baseColor]
+        shimmerLayer.locations = [-0.5, -0.25, 0]
+
+        layer.addSublayer(shimmerLayer)
+
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [-0.5, -0.25, 0]
+        animation.toValue = [1, 1.25, 1.5]
+        animation.duration = 1.2
+        animation.repeatCount = .infinity
+        animation.isRemovedOnCompletion = false
+
+        shimmerLayer.add(animation, forKey: "shimmerAnimation")
+    }
+
+    func removeSkeletonShimmer() {
+        layer.sublayers?.removeAll { $0.name == UIView.skeletonLayerName }
+    }
+
+    func updateSkeletonFrame() {
+        layer.sublayers?
+            .filter { $0.name == UIView.skeletonLayerName }
+            .forEach { $0.frame = bounds }
+    }
+}
+
+// MARK: - Gradient
 extension UIView {
     func addBottomGradient() {
         layer.sublayers?.forEach { sublayer in
-            if sublayer is CAGradientLayer {
+            if sublayer is CAGradientLayer,
+               sublayer.name != UIView.skeletonLayerName {
                 sublayer.removeFromSuperlayer()
             }
         }
